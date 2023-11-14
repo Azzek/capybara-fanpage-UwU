2e4f761c6af54d9d1607baf0f12e5fd97450ab3c
Azzek <kolosowatykolos@gmail.com>
2023-11-14 19:25:53 +0100

    some changes ;3

--- scripts/capybaras-slider.js
+++ scripts/capybaras-slider.js
@@ -5,9 +5,12 @@ capybaras.forEach((capybara) => {
     
 
     HTML += `
-    <div class="capy-class-container">
-        <div class="capy-class-image">
+    <div class="capy-class-container js-capy-class-container">
+        <div class="" data-name="${capybara.name}">
             <img class="capy-class-image" src="resources/capybaras/${capybara.image}"/>
+            <div class="capy-informations-popup js-capy-informations-popup-${capybara.name} ">
+                <img src="resources/capybaras/${capybara.image}"/>
+            </div>
         </div>
         <div class="capy-class-description">
             <div class="capy-name">
@@ -24,5 +27,8 @@ document.querySelector('.capibara-classes').innerHTML = HTML;
 
 
 
+const capybaraClasses = document.querySelectorAll('.capy-class-container')
 
-
+capybaraClasses.forEach((class) => {
+    class.addEventListener()
+})
