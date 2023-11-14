9c76de8e070c4b270bb1f425c2cdb7cef6f3069a
Azenta <jan-chajecki2005@gmail.com>
2023-11-14 23:09:49 +0100

    start adding interactive to capibara classes

--- scripts/capybaras-slider.js
+++ scripts/capybaras-slider.js
@@ -5,9 +5,12 @@ capybaras.forEach((capybara) => {
     
 
     HTML += `
-    <div class="capy-class-container">
-        <div class="capy-class-image">
-            <img class="capy-class-image" src="resources/capybaras/${capybara.image}"/>
+    <div class="capy-class-container js-capy-class-container" data-capi-id="${capybara.name}">
+        <div class="" >
+            <img class="capy-class-image" src="resources/capybaras/${capybara.image}" data-capi-id="${capybara.name}"/>
+            <div class="capy-informations-popup js-capy-informations-popup-${formatCapyNames(capybara.name)}">
+                <img src="resources/capybaras/${capybara.image}"/>
+            </div>
         </div>
         <div class="capy-class-description">
             <div class="capy-name">
@@ -22,7 +25,35 @@ capybaras.forEach((capybara) => {
 document.querySelector('.capibara-classes').innerHTML = HTML;
 
 
+const capybaraClasses = document.querySelectorAll('.capy-class-container')
+
+let isSomePopup = 'gamer-Capybara';
+capybaraClasses.forEach((capyClass) => {
+
+    capyClass.addEventListener('click', () => {
+
+        
+      
+        const classId = capyClass.dataset.capiId
+        
+       const popup = document.querySelector(`.js-capy-informations-popup-${formatCapyNames(classId)}`)
+       popup.classList.toggle('displayNone');
+
+       
+
+       if(isSomePopup !== classId){
+        document.querySelector(`.js-capy-informations-popup-${formatCapyNames(isSomePopup)}`).classList.remove('displayNone');
+        console.log(isSomePopup)
+        
+    }
+    isSomePopup = classId;
+    })
 
 
+    
+})
 
 
+function formatCapyNames(capyName) {
+    return capyName.replace(/ /g, "-");
+}
