b8bd8b45e73e05b8f7a82e552556451414d5c657
Azzek <kolosowatykolos@gmail.com>
2023-11-15 17:34:45 +0100

    mid version capibara classes interactiv

--- data/capybaras.js
+++ data/capybaras.js
@@ -1,38 +1,38 @@
    const capybaras = [ {
     name: 'Capybara Gigachad',
     image: 'capybara-gigachad.jpg',
-    description: 'Just a one of two basic capybara class'
+    description: 'With biceps the size of melons and a chiseled physique, the Capybara Gigachad is the pinnacle of capybara masculinity. The envy of its peers, this capy struts confidently along the river, flexing its impressive muscles and exuding an air of capybara charisma that makes waves among the female capys.'
 },{
     name: 'Capybara Warrior',
     image: 'capybara-warrior.webp',
-    description:'A warrior, very strong capybara'
+    description:'Armored and battle-ready, the Capybara Warrior charges fearlessly into aquatic skirmishes, wielding a sword in one paw and a shield in the other. A master of capybara combat, this warrior defends its territory with unwavering valor, embodying the spirit of a true capybara hero.'
 },{
     name:'Hero Capybara',
     image:'Hero-capybara.jpg',
-    description:'Capybara Hero everyone want a hero like this...'
+    description:'A symbol of capybara justice, the Hero Capybara stands tall, ready to defend its furry brethren from any threat. With a heart full of courage and a cape flowing in the breeze, this capybara leaps into action, rescuing critters in distress and ensuring peace along the riverbanks.'
 },{
     name:'Wizzard Capybara',
     image:'capybara-wiard.jpg',
-    description:'Wizzard Capybara wave a very strong magic abilities'
+    description:'Clad in a robe adorned with mystical symbols, the Wizzard Capybara practices the ancient art of capybara magic. Casting spells with a flick of its tail and brewing potions in a hidden riverbank cauldron, this capybara delves into the arcane, blending the mystical and the aquatic in a harmonious dance.'
 },{
     name:'Sigma Capybara',
     image:'capybara-sigma.jpg',
-    description:'Just a one of two basic capybara class'
+    description:'The Sigma Capybara roams the capybara society as a lone wolf, or rather, a lone rodent. Unbound by societal norms, this capy marches to the beat of its own drum, embodying the essence of capybara individualism with a stoic demeanor and an air of mysterious independence.'
 },{
     name:'Capybara Batman',
     image:'capybara-batman.jpg',
-    description:'Gotham is save'
+    description:'In the shadows of the capybara city, the Capybara Batman emerges as a vigilant guardian. Cloaked in darkness and armed with a utility belt of capybara gadgets, this capy protects its fellow rodents from the nocturnal threats that lurk beyond the waters edge.'
 },{
     name:'gamer Capybara',
     image:'capybara-gamer.jpg',
-    description:'NO-life'
+    description:'Equipped with a headset and a controller, the Gamer Capybara dominates the gaming scene with lightning-fast reflexes and an insatiable appetite for victory. Whether conquering the virtual jungles or racing through pixelated landscapes, this capy is a true digital champion, and its war cry is the click-clack of gaming keys.'
 },{
     name:'EMO Capybara',
     image:'emo-capybara.jpg',
-    description:'i have depression'
+    description:'The EMO Capybara stands alone by the riverbank, its fur draping over its eyes as it contemplates the profound depths of capybara existence. Its melancholic aura and poetic disposition make it the misunderstood bard of the capybara world, expressing emotions through both squeaks and sighs.'
 },{
     name:'businessman Capybara',
     image:'capybara-buisnesmen.jpg',
-    depression:'have so many cash'
+    description:'Always dressed in a sharp suit, the Businessman Capybara is the epitome of corporate success in the capybara world. With a briefcase in one paw and a steely gaze, this capy means serious business, sealing deals with a charismatic charm that rivals its aquatic prowess.'
 },
 ]
