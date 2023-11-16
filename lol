df10027e5e239633149e0e16c7dfcc1809d3351f
Azzek <kolosowatykolos@gmail.com>
2023-11-16 18:59:14 +0100

    #

---
+++ lol
@@ -0,0 +1,15 @@
+<?php
+$liczba1 = 3;
+$liczba2 = 4;
+$operator = '-';
+
+
+    switch($operator){
+        case '-':
+            echo "wynik =".$liczba1 - $liczba2;
+            break;
+    }
+
+
+
+?>
--- scripts/interactiveCapybars.js
+++ scripts/interactiveCapybars.js
@@ -20,18 +20,18 @@ capybaras.forEach((capybara) => {
     </div>`;
 
     showCapibaraHTML += `
-                <div class="capy-informations-popup js-capy-informations-popup-${formatCapyNames(capybara.name)}">
+                <div class="capibara-to-show js-capibara-to-show-${formatCapyNames(capybara.name)}">
                     <img src="resources/capybaras/${capybara.image}"/>
                     <h1>${capybara.name}</h1>
                     <span>${capybara.description}</span>
                 </div>`
 })
 document.querySelector('.capibara-classes').innerHTML = HTML;
-document.querySelector('.show-chosen-capibara').innerHTML = showCapibaraHTML;
+document.querySelector('.show-chosen-capibara-container').innerHTML = showCapibaraHTML;
 
 
 
-let oldCapybara  = 'gamer-Capybara';
+let oldShowedCapibara  = 'gamer-Capybara';
 
 const capybaraClasses = document.querySelectorAll('.capy-class-container')
 
@@ -39,24 +39,24 @@ capybaraClasses.forEach((capyClass) => {
 
     capyClass.addEventListener('click', () => {
         if (localStorage.getItem('chosenCapibara')) {
-          document.querySelector(`.js-capy-informations-popup-${JSON.parse(localStorage.getItem('chosenCapibara'))}`).classList.remove('displayBlock', 'active') 
+          document.querySelector(`.js-capibara-to-show-${JSON.parse(localStorage.getItem('chosenCapibara'))}`).classList.remove('displayBlock', 'active') 
           || confirm
         }
         const classId = capyClass.dataset.capiId
         
-       const popup = document.querySelector(`.js-capy-informations-popup-${formatCapyNames(classId)}`)
-       popup.classList.toggle('displayBlock');
-       popup.classList.toggle('active');
+       const capibaraToShow = document.querySelector(`.js-capibara-to-show-${formatCapyNames(classId)}`)
+       capibaraToShow.classList.toggle('displayBlock');
+       capibaraToShow.classList.toggle('active');
        
 
        
 
-       if(oldCapybara  !== classId){
-            const popupOld = document.querySelector(`.js-capy-informations-popup-${formatCapyNames(oldCapybara )}`)
-            popupOld.classList.remove('displayBlock');
-            popupOld.classList.remove('active');
+       if(oldShowedCapibara !== classId){
+            const ShowedCapibara = document.querySelector(`.js-capibara-to-show-${formatCapyNames(oldShowedCapibara)}`)
+            ShowedCapibara.classList.remove('displayBlock');
+            ShowedCapibara.classList.remove('active');
         }
-    oldCapybara = classId;
+        oldShowedCapibara = classId;
 
     saveCapybaraToLocalStorage(classId)
     })
@@ -68,7 +68,7 @@ capybaraClasses.forEach((capyClass) => {
 
 
     if(localStorage.getItem('chosenCapibara')) {
-        document.querySelector(`.js-capy-informations-popup-${JSON.parse(localStorage.getItem('chosenCapibara'))}`).classList.add('displayBlock', 'active')
+        document.querySelector(`.js-capibara-to-show-${JSON.parse(localStorage.getItem('chosenCapibara'))}`).classList.add('displayBlock', 'active')
     }
 
     
